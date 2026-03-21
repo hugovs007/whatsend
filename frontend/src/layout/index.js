f4e6e59f10a39abf840754190c5710f59cf1e950
hugovs007 <hugovinicius.27@hotmail.com>
2026-03-21 16:27:38 -0300

    refeito conexao do supabase-01

--- frontend/src/layout/index.js
+++ frontend/src/layout/index.js
@@ -232,7 +232,7 @@ const LoggedInLayout = ({ children }) => {
             noWrap
             className={classes.title}
           >
-            whatsend
+            WhatSend
           </Typography>
 
           <div className={classes.themeSwitchContainer}>
