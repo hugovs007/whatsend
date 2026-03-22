d8121e62e735a26f9a9b7616126489c74fe3061e
hugovs007 <hugovinicius.27@hotmail.com>
2026-03-21 21:03:20 -0300

    Render de DB_PASS >> DB_PASSWORD

--- backend/dist/database/seeds/20200904070004-create-default-users.js
+++ backend/dist/database/seeds/20200904070004-create-default-users.js
@@ -5,7 +5,7 @@ module.exports = {
         return queryInterface.bulkInsert("Users", [
             {
                 name: "Administrador",
-                email: "admin@whatsend.com",
+                email: "admin@WhatSend.com",
                 passwordHash: "$2a$08$WaEmpmFDD/XkDqorkpQ42eUZozOqRCPkPcTkmHHMyuTGUOkI8dHsq",
                 profile: "admin",
                 tokenVersion: 0,
--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: process.env.DB_DIALECT || 'postgres',
   host: process.env.DB_HOST || 'aws-1-us-east-1.pooler.supabase.com',
   username: process.env.DB_USER || 'postgres.xhepyqsasoudtreiltxk',
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'postgres',
   port: process.env.DB_PORT || 6543,
   dialectOptions: {
